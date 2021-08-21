91b294130000e50b0df40d0a97b6196c27281260
이영준 <=>
2021-08-21 23:03:40 +0900

    clone RentCompany

---
+++ src/main/java/rentcompany/car/AbstractCar.java
@@ -0,0 +1,16 @@
+package rentcompany.car;
+
+public abstract class AbstractCar implements Car {
+    private double distance;
+    private double distancePerLiter;
+
+    public AbstractCar(double distance, double distancePerLiter){
+        this.distance = distance;
+        this.distancePerLiter = distancePerLiter;
+    }
+
+    public double getChargeQuantity() {
+        return distance / distancePerLiter;
+    }
+
+}
