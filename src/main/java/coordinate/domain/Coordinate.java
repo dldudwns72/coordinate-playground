2bb5eacd0adbff21fbbed8211c8c740f15f705dc
LeeYoungJun <dldudwns72@gmail.com>
2021-08-22 21:08:02 +0900

    coordinate get x,y coordinate

--- src/main/java/coordinate/domain/Coordinate.java
+++ src/main/java/coordinate/domain/Coordinate.java
@@ -2,19 +2,33 @@ package coordinate.domain;
 
 import coordinate.coordinateException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coordinate {
 
     private String coordinate;
 
-    public Coordinate(String coordinate){
+    public Coordinate(String coordinate) {
         valid(coordinate);
         this.coordinate = coordinate;
     }
 
-    private void valid(String coordinate){
-        if(!coordinate.contains("(") || !coordinate.contains(")") || !coordinate.contains(",")){
+    private void valid(String coordinate) {
+        if (!coordinate.contains("(") || !coordinate.contains(")") || !coordinate.contains(",")) {
             throw new coordinateException("`(`, `)`, `,` 특수문자를 제외한 특수문자는 사용할 수 없습니다.");
         }
     }
 
+    public List<Integer> generate() {
+        List<Integer> coordinateArray = new ArrayList<>();
+
+        int x = Integer.parseInt(coordinate.substring(coordinate.indexOf("(") + 1, coordinate.indexOf(",")));
+        int y = Integer.parseInt(coordinate.substring(coordinate.indexOf(",") + 1, coordinate.indexOf(")")));
+
+        coordinateArray.add(x);
+        coordinateArray.add(y);
+
+        return coordinateArray;
+    }
 }
--- src/test/java/coordinatetest/CoordinateTest.java
+++ src/test/java/coordinatetest/CoordinateTest.java
@@ -5,6 +5,8 @@ import coordinate.domain.Coordinate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -17,6 +19,19 @@ public class CoordinateTest {
             String coordinateInput = "(10*10)";
             Coordinate coordinate = new Coordinate(coordinateInput);
         }).isInstanceOf(coordinateException.class);
+
+    }
+
+    @Test
+    @DisplayName("좌표 생성")
+    public void generateCoordinate() {
+        String coordinateInput = "(10,10)";
+        Coordinate coordinate = new Coordinate(coordinateInput);
+
+        List<Integer> coordinateValue = coordinate.generate();
+
+        assertThat(coordinateValue.get(0)).isEqualTo(10);
+        assertThat(coordinateValue.get(1)).isEqualTo(10);
 
     }
 
