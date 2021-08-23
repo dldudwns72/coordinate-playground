5194bc69dd8a44617965d57db66c43ed71bf5fab
LeeYoungJun <dldudwns72@gmail.com>
2021-08-24 02:01:57 +0900

    printf를 이용한 간격맞춰 그래프 그리기

--- src/main/java/coordinate/main.java
+++ src/main/java/coordinate/main.java
@@ -1,9 +1,13 @@
 package coordinate;
 
 import coordinate.ui.InputView;
+import coordinate.ui.ResultView;
 
 public class main {
     public static void main(String[] args){
+        //(10,10)-(14,15)
         InputView.generateCoordinate();
+
+        ResultView.drawingCoordinateGraph();
     }
 }
--- src/main/java/coordinate/ui/ResultView.java
+++ src/main/java/coordinate/ui/ResultView.java
@@ -1,4 +1,53 @@
 package coordinate.ui;
 
 public class ResultView {
+    private static final int MAX_COORDINATE = 24;
+    private static final int MIN_COORDINATE = 0;
+
+    public static void drawingCoordinateGraph() {
+        System.out.println("실행결과");
+
+        for (int i = MAX_COORDINATE; i > 0; i--) {
+            printYCoordinateMultiplesOfTwo(i);
+        }
+
+        System.out.printf("%4s","+");
+
+        for (int i = 0; i < MAX_COORDINATE; i++) {
+            System.out.print("ㅡ");
+        }
+
+        System.out.println("");
+        System.out.print("   ");
+
+
+        for (int i = 1; i <= MAX_COORDINATE; i++) {
+            printXCoordinateMultiplesOfTwo(i);
+        }
+
+
+    }
+
+    private static void printYCoordinateMultiplesOfTwo(int coordinate) {
+
+        if (coordinate % 2 == 0) {
+            System.out.printf("%3d",coordinate);
+            System.out.println("ㅣ");
+        }
+
+        if (coordinate % 2 != 0) {
+            System.out.printf("%5s","ㅣ\n" );
+        }
+
+    }
+
+    private static void printXCoordinateMultiplesOfTwo(int coordinate) {
+        if (coordinate % 2 == 0) {
+            System.out.printf("%d",coordinate);
+        }
+
+        if (coordinate % 2 != 0) {
+            System.out.print(" ");
+        }
+    }
 }
